ca74e98c6cae36bb93720175c644deb3c364b3be
xinyuexrow <wang@xrow.de>
2018-03-27 18:45:07 +0200

    first commit

---
+++ eventtypes/event/publishsignal/publishsignaltype.php
@@ -0,0 +1,46 @@
+<?php
+
+use eZ\Publish\Core\SignalSlot\Signal\ContentService\PublishVersionSignal;
+
+/**
+ * Emit a new stack publish version signal after content is published in legacy.
+ * This signal can be detected by a Signal Listener (see ../../../../../EventListener/PublishVersionListener.php).
+ */
+class PublishSignalType extends eZWorkflowEventType
+{
+    const WORKFLOW_TYPE_STRING = "publishsignal";
+
+    public function __construct()
+    {
+        $this->eZWorkflowEventType(PublishSignalType::WORKFLOW_TYPE_STRING, "Publish content signal");
+        $this->setTriggerTypes([
+            'content' => [
+                'publish' => [
+                    'after'
+                ]
+            ]
+        ]);
+    }
+
+    public function execute($process, $event)
+    {
+        $parameters = $process->attribute('parameter_list');
+        $container = ezpKernel::instance()->getServiceContainer();
+        $logger = $container->get('monolog.logger.xrow_db_integrity');
+        $signalDispatcher = $container->get('ezpublish.signalslot.signal_dispatcher');
+
+        $logger->debug('Emitting PublishVersionSignal for content ID: ' . $parameters['object_id'] . ", version " . $parameters['version']);
+        $signalDispatcher->emit(
+            new PublishVersionSignal(
+                array(
+                    'contentId' => $parameters['object_id'],
+                    'versionNo' => $parameters['version'],
+                )
+            )
+        );
+
+        return PublishSignalType::STATUS_ACCEPTED;
+    }
+}
+
+eZWorkflowEventType::registerEventType(PublishSignalType::WORKFLOW_TYPE_STRING, "PublishSignalType");
---
+++ settings/workflow.ini.append.php
@@ -0,0 +1,7 @@
+<?php /*
+
+[EventSettings]
+ExtensionDirectories[]=xrowworkflowbridge
+AvailableEventTypes[]=event_publishsignal
+
+*/
